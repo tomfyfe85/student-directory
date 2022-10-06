e8b00ddf8eb670c08a131f3b61556dec71d142e6
tomfyfe <tomfyfe85@gmail.com>
2022-10-06 21:31:23 +0100

    added input_students method

--- directory.rb
+++ directory.rb
@@ -26,9 +26,28 @@ end
 end
 
 def print_footer(names)
-    puts "Overall, we have #{names.count} great students"
+    puts "Overall, we have #{names.count} evil students"
 end 
 
+def input_students
+  puts "please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #create an empty array
+  students = []
+  #get the first name
+  name = gets.chomp 
+  #while the name is not empty repeat this code
+  while !name.empty? do 
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from user
+    name = gets.chomp
+  end
+  #return array of students
+  students
+end 
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
