364d5cdb32936977840b5c1b436c087d65d4fc35
tomfyfe <tomfyfe85@gmail.com>
2022-10-09 16:48:16 +0100

    added interactice menu

--- directory.rb
+++ directory.rb
@@ -15,21 +15,24 @@ students = [
 
 
 def print_header
+  puts
   puts "The students of Villains Academy"
   puts "--------------"
 end
 
 def print(names)
 names.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-end
+  puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end 
 end
 
 def print_footer(names)
     puts "Overall, we have #{names.count} evil students"
+    puts
 end 
 
 def input_students
+  puts
   puts "please enter the names of the students"
   puts "To finish, just hit return twice"
   #create an empty array
@@ -47,6 +50,30 @@ def input_students
   students
 end 
 
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students" 
+    puts "9. Exit"
+    selection = gets.chomp
+
+    case selection 
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit 
+    else 
+      puts "I don't know what you mean, try again"
+    end
+  end
+end
+
+interactive_menu
 students = input_students
 print_header
 print(students)
