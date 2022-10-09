e5f12a6ade9de86c6124d7462b110357f0b4590b
tomfyfe <tomfyfe85@gmail.com>
2022-10-09 17:16:16 +0100

    refactoring

--- directory.rb
+++ directory.rb
@@ -1,81 +1,86 @@
-# first we print the list of students
-students = [
- {name: "Dr. Hannibal Lecter", cohort: :november},
- {name:"Darth Vader",  cohort: :november},
- {name:"Nurse Ratched",  cohort: :november},
- {name:"Michael Corleone",  cohort: :november},
- {name:"Alex DeLarge", cohort: :november},
- {name:"The Wicked Witch of the West",  cohort: :november},
- {name:"The Terminator", cohort: :november},
- {name:"The Joker", cohort: :november},
- {name:"Joffrey Baratheon", cohort: :november},
- {name:"Norman Bates", cohort: :november},
-]
 
+@students = []
 
+# students = [
+#  {name: "Dr. Hannibal Lecter", cohort: :november},
+#  {name:"Darth Vader",  cohort: :november},
+#  {name:"Nurse Ratched",  cohort: :november},
+#  {name:"Michael Corleone",  cohort: :november},
+#  {name:"Alex DeLarge", cohort: :november},
+#  {name:"The Wicked Witch of the West",  cohort: :november},
+#  {name:"The Terminator", cohort: :november},
+#  {name:"The Joker", cohort: :november},
+#  {name:"Joffrey Baratheon", cohort: :november},
+#  {name:"Norman Bates", cohort: :november},
+# ]
+def input_students
+  puts
+  puts "please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #get the first name
+  name = gets.chomp 
+  #while the name is not empty repeat this code
+  while !name.empty? do 
+    students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    # get another name from user
+    name = gets.chomp
+  end
+end 
 
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end 
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students" 
+  puts "9. Exit"
+end 
+
+def show_students
+  print_header
+  print(students)
+  print_footer(students)
+end 
+
+def process(selection)
+  case selection 
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+    else 
+    puts "I don't know what you mean, try again"
+  end
+end
+  
 def print_header
   puts
   puts "The students of Villains Academy"
   puts "--------------"
 end
 
-def print(names)
-names.each do |student|
+def print_student_list
+  @student.each do |student|
   puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end 
 end
 
-def print_footer(names)
-    puts "Overall, we have #{names.count} evil students"
-    puts
-end 
-
-def input_students
+def print_footer
+  puts "Overall, we have #{@students.count} evil students"
   puts
-  puts "please enter the names of the students"
-  puts "To finish, just hit return twice"
-  #create an empty array
-  students = []
-  #get the first name
-  name = gets.chomp 
-  #while the name is not empty repeat this code
-  while !name.empty? do 
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    # get another name from user
-    name = gets.chomp
-  end
-  #return array of students
-  students
 end 
 
-def interactive_menu
-  students = []
-  loop do
-    puts "1. Input the students"
-    puts "2. Show the students" 
-    puts "9. Exit"
-    selection = gets.chomp
-
-    case selection 
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit 
-    else 
-      puts "I don't know what you mean, try again"
-    end
-  end
-end
 
 interactive_menu
 students = input_students
 print_header
-print(students)
-print_footer(students)
+print(@students)
+print_footer(@students)
 
